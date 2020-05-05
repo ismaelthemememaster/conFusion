43c9a162390f8c57056c633b6fd20577f2256201
Ismael Restrepo Vélez <irestrepov@unal.edu.co>
2020-05-04 21:23:56 -0500

    cosa
    La tarea esa pero tengo problemas con los containers

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
         function RenderComments({comments}){
 
@@ -59,9 +61,29 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
             console.log("Cada renderizada que hace este codigo me quita dos meses de vida");
             
             if (props.dish != null)
-                return(
-                    <RenderDish dish= {props.dish} />
-                );
+            return (
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                    </div>
+                </div>
+                </div>
+            );
             else
                 return(
                     <div></div>
