8bd71444dcdb18d97b24f092e90bf474a092601e
Ismael Restrepo Vélez <irestrepov@unal.edu.co>
2020-05-04 20:42:44 -0500

    Single Page Applications Part 1
    El home ya muestras tres cosas pero no tienen OnClick

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -4,7 +4,11 @@ import Home from './HomeComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -14,8 +18,10 @@ class Main extends Component {
     super(props);
 
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -24,8 +30,11 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-          <Home 
-          />
+        <Home 
+        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+      />
       );
     }
 
@@ -35,6 +44,7 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />} />
               <Redirect to="/home" />
           </Switch> 
         <Footer />
