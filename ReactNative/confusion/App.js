1f30c6460987721a6a2f78edfcb666b3149a352c
Ismael Restrepo Vélez <irestrepov@unal.edu.co>
2020-05-20 11:19:27 -0500

    Components Part 1
    Al final me faltaba importar una wea y sho como imbesilo perdiendo el tiempo por no saber leer

--- ReactNative/confusion/App.js
+++ ReactNative/confusion/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
+import { StyleSheet} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
