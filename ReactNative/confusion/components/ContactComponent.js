6ea1b1b27fc84642d78228bcbe09ef46d3a2f0e7
Ismael Restrepo Vélez <irestrepov@unal.edu.co>
2020-06-01 16:06:04 -0500

    Animatable
    Ya todo tiene animacionsitas chidas

--- ReactNative/confusion/components/ContactComponent.js
+++ ReactNative/confusion/components/ContactComponent.js
@@ -2,24 +2,27 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Card } from 'react-native-elements';
 import { Divider } from 'react-native-elements'
+import * as Animatable from 'react-native-animatable';
 
 function RenderContact() {
 
     return(
-        <Card>
-            <Text style={ { fontWeight: 'bold',  textAlign: "center" }}    >Contact Information{"\n"}
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>                
+            <Card title='Contact Information'>
+                <Text style={ { fontWeight: 'bold',  textAlign: "center" }}    >Contact Information{"\n"}
+                    </Text>
+                    <Divider style={{ backgroundColor: 'gainsboro' }} />
+                <Text style={{margin: 10}}>
+                {"\n"}
+                121, Clear Water Bay Road {"\n"}{"\n"}
+                Clear Water Bay, Kowloon{"\n"}{"\n"}
+                HONG KONG{"\n"}{"\n"}
+                Tel: +852 1234 5678{"\n"}{"\n"}
+                Fax: +852 8765 4321{"\n"}{"\n"}
+                Email:confusion@food.net
                 </Text>
-                <Divider style={{ backgroundColor: 'gainsboro' }} />
-            <Text style={{margin: 10}}>
-            {"\n"}
-            121, Clear Water Bay Road {"\n"}{"\n"}
-            Clear Water Bay, Kowloon{"\n"}{"\n"}
-            HONG KONG{"\n"}{"\n"}
-            Tel: +852 1234 5678{"\n"}{"\n"}
-            Fax: +852 8765 4321{"\n"}{"\n"}
-            Email:confusion@food.net
-            </Text>
-        </Card>
+            </Card>
+        </Animatable.View>
     );
 
 }
