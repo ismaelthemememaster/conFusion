15369003f75b0391a1d45034d819aee3df3ba03b
Ismael Restrepo Vélez <irestrepov@unal.edu.co>
2020-06-09 14:24:17 -0500

    Voy a empezar la tarea
    Voy a mover cosas

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -48,7 +48,10 @@ app.use((req, res, next) => {
   console.log(req.headers);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>Index chan doesnt love you</h1></body></html>');
+  
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
 
 });
 
