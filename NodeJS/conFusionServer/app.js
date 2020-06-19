e82ad6e04dbff51a4b614ac3098ab68d5eee852d
Ismael Restrepo Vélez <irestrepov@unal.edu.co>
2020-06-18 21:32:58 -0500

    File Upload
    Se suben imagenes jpg, png y gf (EN MINUSCULAS) con un post en https://localhost:3443/imageUpload, pero tiene que estar logueado como admin y quitar la opcion de que pedir el ssl lo que sea

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -7,14 +7,14 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-var config = require('./config');
-
+const uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -62,6 +62,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
